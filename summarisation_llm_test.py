acf64edb463d3f421d0bb84a84e67a4bfe1bfd1a
Konstantin Perikov <pericov.ko@gmail.com>
2023-08-03 21:38:24 +0100

    feat: add multiple models and file saving results

--- summarisation_llm_test.py
+++ summarisation_llm_test.py
@@ -1,24 +1,37 @@
 import json
 
-from transformers import pipeline
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+# List of model names to use
+model_names = ["facebook/bart-large-cnn", "google/flan-t5-large", "google/pegasus-xsum"]
 
-# Load the JSON data from a file
-with open("data/1.json", "r") as f:
-    data = json.load(f)
+# List of JSON files to process
+json_files = ["data/1.json", "data/2.json", "data/3.json"]
 
-# Extract the texts
-texts = [item[1] for item in data]  # item[1] corresponds to "text"
+results = {}
 
-# Join the texts into a single string
-joined_text = " ".join(texts)
+for model_name in model_names:
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
-# Join the texts into a single string
-joined_text = " ".join(texts)
+    for json_file in json_files:
+        with open(json_file, "r") as f:
+            data = json.load(f)
 
-# Summarize the joined text
-summary = summarizer(joined_text, max_length=150, min_length=30, do_sample=False)
+        texts = [item[1] for item in data]  # item[1] corresponds to "text"
+        joined_text = " ".join(texts)
 
-print("Summary:")
-print(summary[0]["summary_text"])
+        tokens_input = tokenizer.encode(
+            "summarize: " + joined_text, return_tensors="pt", truncation=True
+        )
+        ids = model.generate(tokens_input, min_length=30, max_length=300)
+        summary = tokenizer.decode(ids[0], skip_special_tokens=True)
+
+        # Store the result
+        if model_name not in results:
+            results[model_name] = {}
+        results[model_name][json_file] = summary
+
+# Save results to a JSON file
+with open("results.json", "w") as f:
+    json.dump(results, f)
